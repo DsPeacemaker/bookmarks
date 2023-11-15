fa0a6adbdb169e78bf48be22357d05fae1c3186c
DsPeacemaker <enotikpartizan@mail.ru>
2023-11-15 17:02:56 +0300

    feat: Перенаправление после аутентификации

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+    # предыдущий url входа
     path('login/', views.use_login, name='login'),
+
+    # url адресс входа и выхода
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', views.dashboard, name='dashboard'),
 ]
+
