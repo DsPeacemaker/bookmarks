5d29a29983f5be42c64ae5245536f3cdb626f8fc
DsPeacemaker <enotikpartizan@mail.ru>
2023-11-15 18:48:41 +0300

    feat: смена пароля

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -9,6 +9,15 @@ urlpatterns = [
     # url адресс входа и выхода
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    # url-адреса смены пароля
+    path('password-change/',
+         auth_views.PasswordChangeView.as_view(),
+         name='password-change'),
+    path('password-change/done/',
+         auth_views.PasswordChangeView.as_view(),
+         name='password_change_done'),
     path('', views.dashboard, name='dashboard'),
+
 ]
 
