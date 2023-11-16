60e6e7c650a2d7673d2e6d04bf5224a5836c355c
DsPeacemaker <enotikpartizan@mail.ru>
2023-11-16 22:20:16 +0300

    feat: Редактирование пользователем полей в своем аккаунте (url edit profile)

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     # предыдущий url входа
-    path('login/', views.use_login, name='login'),
+    path('login/', views.user_login, name='login'),
 
     # url адресс входа и выхода
     path('login/', auth_views.LoginView.as_view(), name='login'),
@@ -29,12 +29,13 @@ urlpatterns = [
          auth_views.PasswordResetConfirmView.as_view(),
          name='password_reset_confirm'),
     path('password-reset/complete/',
-         auth_views.PasswordResetCompleteView.as_view(),
-         name='password_reset_complete'),
+          auth_views.PasswordResetCompleteView.as_view(),
+          name='password_reset_complete'),
 
-   # path('', include('django.contrib.auth.urls')),
+    #path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
     path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit')
 
 ]
 
